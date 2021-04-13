819aa78e5bc756bd87f18abcfce2a106b4c8ef5f
phamminhtan <tanpm@mpos.vn>
2021-04-13 14:51:54 +0700

    fix error with google driver

--- src/main/java/com/phamtan/cuu_tro/CuuTroApplication.java
+++ src/main/java/com/phamtan/cuu_tro/CuuTroApplication.java
@@ -1,11 +1,10 @@
 package com.phamtan.cuu_tro;
 
-import com.google.api.client.http.javanet.NetHttpTransport;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
